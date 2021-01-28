32042aad9bb4f12f86e05a606622602895365c0d
enamulhaque7 <enamulhaquemb01@gmail.com>
2021-01-28 23:29:26 +0600

    update

---
+++ payoneerBank.js
@@ -0,0 +1,11 @@
+//Login And Jump Dashboard Section
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", function(){
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+})
+//Calculate
+
